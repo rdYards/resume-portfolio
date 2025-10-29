baf357497db994c985155a0a0eee423df7663c0e
rdYards <FASN-CV-Voyager@proton.me>
2025-10-29 15:21:31 -0500

    Added credits to others

--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -1,8 +1,15 @@
 export function NotFound() {
-	return (
-		<section>
-			<h1>404: Not Found</h1>
-			<p>It's gone :(</p>
-		</section>
-	);
+  return (
+    <section>
+      <h2>404</h2>
+      <h2>Woops you found where I store my thanks.</h2>
+      <h3>Special thanks to the following websites for inspiration.</h3>
+      <a href="https://robertsspaceindustries.com/en/" class="button-secondary">
+        robertsspaceindustries.com
+      </a>
+      <a href="https://maddieharris.org/" class="button-secondary">
+        maddieharris.org
+      </a>
+    </section>
+  );
 }
