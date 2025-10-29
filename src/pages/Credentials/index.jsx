4e657c1a608e15664f6fd1c656e1f4c1cd0094f7
rdYards <FASN-CV-Voyager@proton.me>
2025-10-29 15:19:46 -0500

    Added Creds to Credentials page

--- src/pages/Credentials/index.jsx
+++ src/pages/Credentials/index.jsx
@@ -1,6 +1,35 @@
-import preactLogo from '../../assets/preact.svg';
-import './style.css';
+import "./style.css";
+import { CredBanner } from "../../components/CredBanner/CredBanner";
 
 export function Credentials() {
-  return (<></>);
+  return (
+    <section>
+      <CredBanner
+        mode="light"
+        title="Bachelor of Science in Computer Science"
+        active_date="May 2026"
+        image="src/assets/cofo_logo.png"
+      />
+      <CredBanner
+        mode="light"
+        title="Minor in Cybersecurity"
+        active_date="May 2026"
+        image="src/assets/cofo_logo.png"
+      />
+      <CredBanner
+        mode="light"
+        title="CompTIA Network+"
+        verify_code="V67GDSW581EEK6S2"
+        active_date="June 2025"
+        image="src/assets/Network+-svg.svg"
+      />
+      <CredBanner
+        mode="light"
+        title="CompTIA Security+"
+        verify_code="B5TR4RNY724ES5M2"
+        active_date="August 2025"
+        image="src/assets/Security+-svg.svg"
+      />
+    </section>
+  );
 }
