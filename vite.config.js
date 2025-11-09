8f163c8d9639bc520e3f54a87da6aef3718de0ec
rdYards <FASN-CV-Voyager@proton.me>
2025-11-08 22:49:25 -0600

    Update vite config and publish workflow to fix routing issue

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import preact from "@preact/preset-vite";
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/resume/" : "/",
   plugins: [preact()],
-  resolve: {
-    alias:
-      process.env.NODE_ENV === "production"
-        ? {
-            "@": "/resume/",
-            src: "/resume/",
-          }
-        : {},
-  },
 });
